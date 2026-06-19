8ffb954a3313f2f9fa687869da23042840c0bec2
tomas-calvo01 <tcalvo246@alumnos.iua.edu.ar>
2026-06-19 15:38:12 -0300

    .

--- src/main/python/caminante.py
+++ src/main/python/caminante.py
@@ -1,8 +1,32 @@
 from compilerVisitor import compilerVisitor
 from compilerParser import compilerParser
+
 class caminante(compilerVisitor):
-    def visitPrograma(self, ctx:compilerParser.ProgramaContext):
-        print("programa procesando")
-        return (ctx)
     
+    def visitPrograma(self, ctx: compilerParser.ProgramaContext):
+        print("Programa procesado")
+        return self.visitChildren(ctx)
     
+    def visitDeclaracion(self, ctx: compilerParser.DeclaracionContext):
+        print("Declaracion: " + ctx.getText())
+        return self.visitChildren(ctx)
+    
+    def visitAsignacion(self, ctx: compilerParser.AsignacionContext):
+        print("Asignacion: " + ctx.getText())
+        return self.visitChildren(ctx)
+    
+    def visitIif(self, ctx: compilerParser.IifContext):
+        print("If encontrado")
+        return self.visitChildren(ctx)
+    
+    def visitIwhile(self, ctx: compilerParser.IwhileContext):
+        print("While encontrado")
+        return self.visitChildren(ctx)
+    
+    def visitIfor(self, ctx: compilerParser.IforContext):
+        print("For encontrado")
+        return self.visitChildren(ctx)
+    
+    def visitFuncion(self, ctx: compilerParser.FuncionContext):
+        print("Funcion: " + ctx.ID().getText())
+        return self.visitChildren(ctx)
